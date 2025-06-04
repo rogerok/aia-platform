3d51cf95196829dbed1e092414d0a8c3484b05f4
vyacheslav <fedorik833@gmail.com>
2025-06-04 15:58:19 +0300

    first version mobx store usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Comp } from './Comp';
+
 interface PageProps {
   className?: string;
 }
 
 const Page = async (props: PageProps) => {
   return (
-    <div className={'flex border-2 text-4xl font-bold text-gray-600'}>Page</div>
+    <div className={'flex border-2 text-4xl font-bold text-gray-600'}>
+      <Comp />
+    </div>
   );
 };
 
--- src/lib/store-adapter/store.tsx
+++ src/lib/store-adapter/store.tsx
@@ -1,38 +1,72 @@
 'use client';
 
-// import { makeAutoObservable } from 'mobx';
-// import React, { createContext, ReactNode, useContext, useRef } from 'react';
+import { makeAutoObservable } from 'mobx';
+import { createContext, FC, ReactNode, useContext, useRef } from 'react';
 
-// class TestStore {
-//   count = 1;
-//
-//   constructor() {
-//     makeAutoObservable(this);
-//   }
-// }
+class TestStore {
+  count = 1;
 
-// function createStoreContext<Store>(makeStore: () => Store) {
-//   const StoreContext = createContext<null | Store>(null);
-//
-//   const useStore = (): Store => {
-//     const contextVal = useContext(StoreContext);
-//
-//     if (!contextVal) {
-//       throw new Error('useStore must be used within a Provider');
-//     }
-//
-//     return contextVal;
-//   };
-//
-//   const Provider = ({ children }: { children: ReactNode }) => {
-//     const ref = useRef<null | Store>(null);
-//
-//     if (!ref.current) {
-//       ref.current = makeStore();
-//     }
-//
-//     return <StoreContext value={ref.current}>{children}</StoreContext>;
-//   };
-//
-//   return { Provider, useStore };
-// }
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increment = (): void => {
+    this.count++;
+  };
+}
+
+export function createMobxContext<StoreType>() {
+  const Context = createContext<StoreType | null>(null);
+
+  const useStore = (): StoreType => {
+    const value = useContext(Context);
+
+    if (!value) {
+      throw new Error('useStore must be used within its Provider');
+    }
+
+    return value;
+  };
+
+  const useStoreHydration = (fn: () => void) => {
+    const state = useStore();
+    const ref = useRef(false);
+
+    // FIXME: put in in useEffect?
+    if (!ref.current) {
+      fn();
+      ref.current = true;
+    }
+
+    return state;
+  };
+
+  const createProvider = (makeStore: () => StoreType) => {
+    const StoreProvider: FC<{ children: ReactNode }> = ({ children }) => {
+      const storeRef = useRef<StoreType | null>(null);
+
+      // FIXME: put in in useEffect?
+      if (!storeRef.current) {
+        storeRef.current = makeStore();
+      }
+
+      return (
+        <Context.Provider value={storeRef.current}>{children}</Context.Provider>
+      );
+    };
+
+    return StoreProvider;
+  };
+
+  return {
+    createProvider,
+    useStore,
+    useStoreHydration,
+  };
+}
+
+const { createProvider, useStore } = createMobxContext<TestStore>();
+
+export const useRootStore = useStore;
+
+export const RootStoreProvider = createProvider(() => new TestStore());
