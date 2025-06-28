c010e321d708c59e6f31ed9f29a0be49ef073100
rogerok <fedorik833@gmail.com>
2025-06-28 18:31:48 +0300

    Add Google OAuth client ID and secret to environment constants and types

--- src/lib/constants/envs.ts
+++ src/lib/constants/envs.ts
@@ -4,4 +4,6 @@ export const envs = {
   dbURl: process.env.NEXT_PUBLIC_DATABASE_URL,
   githubClientId: process.env.GITHUB_CLIENT_ID,
   githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 } as const;
