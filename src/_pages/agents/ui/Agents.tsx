24af5bab5dff1e3cb8d0e09bac33b3596f41cab4
rogerok <fedorik833@gmail.com>
2025-07-18 20:27:35 +0300

    created agents table

--- src/_pages/agents/ui/Agents.tsx
+++ src/_pages/agents/ui/Agents.tsx
@@ -6,6 +6,7 @@ import { FC } from 'react';
 
 import { useAgentsStoreHydration } from '@/_pages/agents/store/AgentsStore';
 import { agentsColumns } from '@/_pages/agents/ui/AgentsTableCol';
+import { EmptyListIndicator } from '@/components/custom/EmptyListIndicator/EmptyListIndicator';
 import { DataTable } from '@/components/table/DataTable';
 import { AgentModel } from '@/lib/models/agents';
 import { AgentRouterOutput } from '@/trpc/server/routers/agents';
@@ -21,12 +22,17 @@ export const Agents: FC<AgentsProps> = observer((props) => {
   );
 
   return (
-    !!store.data.length && (
-      <div
-        className={'bg-muted flex flex-1 flex-col gap-y-4 px-4 pb-4 md:px-8'}
-      >
+    <div className={'bg-muted flex flex-1 flex-col gap-y-4 px-4 pb-4 md:px-8'}>
+      {!store.data.length ? (
         <DataTable columns={agentsColumns} data={store.data} />
-      </div>
-    )
+      ) : (
+        <EmptyListIndicator
+          description={
+            'Create agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call.'
+          }
+          title={'Create your first agent'}
+        />
+      )}
+    </div>
   );
 });
--- src/_pages/agents/ui/AgentsTableCol.tsx
+++ src/_pages/agents/ui/AgentsTableCol.tsx
@@ -1,12 +1,45 @@
 'use client';
 
 import { ColumnDef } from '@tanstack/react-table';
+import { CornerDownRightIcon, VideoIcon } from 'lucide-react';
 
+import { GeneratedAvatar } from '@/components/custom/GeneratedAvatar/GeneratedAvatar';
+import { Badge } from '@/components/ui/badge';
 import { AgentModel } from '@/lib/models/agents';
 
 export const agentsColumns: ColumnDef<AgentModel>[] = [
   {
     accessorKey: 'name',
+    cell: ({ row }) => (
+      <div className={'flex flex-col gap-y-1'}>
+        <div className={'flex items-center gap-x-2'}>
+          <GeneratedAvatar firstName={row.original.name} />
+          <span className={'font-semibold capitalize'}>
+            {row.original.name}
+          </span>
+        </div>
+        <div className={'flex items-center gap-x-2'}>
+          <CornerDownRightIcon className={'text-muted-foreground size-3'} />
+          <span className={'max-w-[200px] truncate capitalize'}>
+            {row.original.instructions}
+          </span>
+        </div>
+      </div>
+    ),
     header: 'Agent Name',
+  },
+  {
+    accessorKey: 'meetingCount',
+    cell: ({ row }) => (
+      <Badge
+        className={'flex items-center gap-x-2 [&>svg]:size-4'}
+        variant={'secondary'}
+      >
+        <VideoIcon />
+        {/*{row.original.meetingCount}*/}5
+        {row.original.meetingCount === 1 ? 'meeting' : 'meetings'}
+      </Badge>
+    ),
+    header: 'Meetings',
   },
 ];
