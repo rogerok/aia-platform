91cbcbaf7214e4b147f3b3f30df63e1dc451d65c
vyacheslav <fedorik833@gmail.com>
2025-07-21 17:26:54 +0300

    setup navigation and params setting

--- src/lib/utils/searchParamsHandler.ts
+++ src/lib/utils/searchParamsHandler.ts
@@ -9,11 +9,11 @@ import { BaseQueryParamsModel } from '@/lib/models/paramsModel';
 export class SearchParamsHandler<T extends BaseQueryParamsModel> {
   cls: new () => T;
 
-  params: T = this.getPlain();
+  params: T;
 
   constructor(cls: new () => T) {
     this.cls = cls;
-
+    this.params = this.getPlain();
     makeAutoObservable(
       this,
       {},
@@ -22,6 +22,10 @@ export class SearchParamsHandler<T extends BaseQueryParamsModel> {
       },
     );
     makeLoggable(this);
+  }
+
+  getParam<K extends keyof T>(key: K): T[K] {
+    return this.params[key];
   }
 
   getPlain(): T {
@@ -36,15 +40,25 @@ export class SearchParamsHandler<T extends BaseQueryParamsModel> {
     });
   }
 
+  pushHistory(params: string) {
+    window.history.pushState({}, '', window.location.pathname + '?' + params);
+  }
+
+  replaceHref(href: string) {
+    window.location.href = href;
+  }
+
   setParam<K extends keyof T>(key: K, value: T[K]) {
     this.params[key] = value;
   }
 
-  setQueryParams(params: T) {
+  setQueryParams(params: T, withRefresh: boolean = true) {
+    this.params = params;
+
     const searchParams = new URLSearchParams();
 
-    Object.keys(params).forEach((key) => {
-      const value = params[key as keyof T];
+    Object.keys(this.params).forEach((key) => {
+      const value = this.params[key as keyof T];
 
       if (value === null || value === undefined) {
         return;
@@ -53,10 +67,12 @@ export class SearchParamsHandler<T extends BaseQueryParamsModel> {
       }
     });
 
-    window.history.pushState(
-      {},
-      '',
-      window.location.pathname + '?' + searchParams.toString(),
-    );
+    if (withRefresh) {
+      this.replaceHref(
+        window.location.pathname + '?' + searchParams.toString(),
+      );
+    } else {
+      this.pushHistory(searchParams.toString());
+    }
   }
 }
